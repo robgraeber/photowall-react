3097d3f9fb2caa418175944f9957e17383216cc5
Rob Graeber <bgraeber@gmail.com>
2014-06-02 22:17:43 -0700

    Basic react view implementation

--- build/app.js
+++ build/app.js
@@ -1,24 +1,49 @@
 /** @jsx React.DOM */
 $(function(){
+  var isScrolledIntoView = function(el){
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(el).offset().top;
+    var elemBottom = elemTop + $(el).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  };
+  var animateCheck = function(el){
+    if(isScrolledIntoView(el)){
+      TweenMax.from(el, 1.2, {rotationY:500, scale:0.1, ease:Power1.easeOut});
+    }
+  };
   var PhotoView = React.createClass({displayName: 'PhotoView',
+    imgError:function(e){
+      e.target.style.display = 'none';
+    },
+    componentDidMount: function() {
+      animateCheck(this.getDOMNode());
+    },
     render: function(){
       return (
-        React.DOM.img( {src:this.props.mediumUrl, className:"photoImg"} )
+        React.DOM.img( {src:this.props.mediumUrl, className:"photoImg", onError:this.imgError})
       );
     }
   });
+
   var PhotoWall = React.createClass({displayName: 'PhotoWall',
     render: function(){
+      var photoItems = {};
+      _.each(photos.data.slice(0,30), function(item, index){
+        photoItems["photo-"+index] = PhotoView( {mediumUrl:item.medium_url} );
+      });
       return (
-        React.DOM.div( {className:"photoWall"}, 
-          PhotoView( {mediumUrl:"http://pbs.twimg.com/media/BjXVRQZCEAAqzdR.png"})
+        React.DOM.div( {className:"photos"}, 
+          photoItems
         )
       );
     }
   });
+
   React.renderComponent(
     PhotoWall(null ),
     document.getElementById('main_region')
   );
-  console.log(photos.data);
+  // console.log(photos.data);
 });
--- src/app.js
+++ src/app.js
@@ -1,24 +1,49 @@
 /** @jsx React.DOM */
 $(function(){
+  var isScrolledIntoView = function(el){
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(el).offset().top;
+    var elemBottom = elemTop + $(el).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  };
+  var animateCheck = function(el){
+    if(isScrolledIntoView(el)){
+      TweenMax.from(el, 1.2, {rotationY:500, scale:0.1, ease:Power1.easeOut});
+    }
+  };
   var PhotoView = React.createClass({
+    imgError:function(e){
+      e.target.style.display = 'none';
+    },
+    componentDidMount: function() {
+      animateCheck(this.getDOMNode());
+    },
     render: function(){
       return (
-        <img src={this.props.mediumUrl} className="photoImg" />
+        <img src={this.props.mediumUrl} className="photoImg" onError={this.imgError}/>
       );
     }
   });
+
   var PhotoWall = React.createClass({
     render: function(){
+      var photoItems = {};
+      _.each(photos.data.slice(0,30), function(item, index){
+        photoItems["photo-"+index] = <PhotoView mediumUrl={item.medium_url} />;
+      });
       return (
-        <div className="photoWall">
-          <PhotoView mediumUrl="http://pbs.twimg.com/media/BjXVRQZCEAAqzdR.png"/>
+        <div className="photos">
+          {photoItems}
         </div>
       );
     }
   });
+
   React.renderComponent(
     <PhotoWall />,
     document.getElementById('main_region')
   );
-  console.log(photos.data);
+  // console.log(photos.data);
 });
